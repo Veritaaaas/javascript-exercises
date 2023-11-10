bb9babeaf98e12a61aee9dd939e00c1f66909037
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2023-11-10 21:24:40 +0800

    done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) 
+{
 
+    for (let i = 0; i < people.length; i++)
+    {
+        if (people[i].yearOfDeath == undefined)
+        {
+            people[i].yearOfDeath = new Date().getFullYear();
+        }
+
+        let age = people[i].yearOfDeath - people[i].yearOfBirth;
+        people[i].age = age;
+    }
+
+    people.sort((a, b) => b.age - a.age);
+    
+    return people[0];
 };
 
-// Do not edit below this line
+// Do not edit below this   
 module.exports = findTheOldest;
